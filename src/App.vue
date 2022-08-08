7d47f8a9659a5c75debd4c8bfd06eaa717c57a17
songhajang <angpan2004@gmail.com>
2022-08-08 17:28:47 +0900

    이벤트 핸들러이용_ 신고버튼 생성

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,26 @@
 
     <h1>{{logo}}</h1>
 
-    <div v-for="oneRomTyp in 3" :key="oneRomTyp">
-      <h4>{{products[oneRomTyp-1]}}</h4>
-      <p>{{price[oneRomTyp-1]}} 만원</p>
+    <div>
+      <h4>{{products[0]}}</h4>
+      <p>{{price[0]}} 만원</p>
+      <button 
+        @click="increase(0)">
+        허위매물신고</button> <span>신고수 : {{신고수[0]}}</span>
+    </div>
+    <div>
+      <h4>{{products[1]}}</h4>
+      <p>{{price[1]}} 만원</p>
+      <button 
+        @click="increase(1)">
+        허위매물신고</button> <span>신고수 : {{신고수[1]}}</span>
+    </div>
+    <div>
+      <h4>{{products[2]}}</h4>
+      <p>{{price[2]}} 만원</p>
+      <button 
+        @click="increase(2)">
+        허위매물신고</button> <span>신고수 : {{신고수[2]}}</span>
     </div>
 
   </div>
@@ -28,7 +45,13 @@ export default {
         menu : ['Home', 'Shop', 'About'],
         products : ['역삼동원룸','청호동원룸','마포구원룸'],
         price : [60,70,80],
+        신고수 : [0,0,0],
     }
+  },
+  methods: {
+    increase(e){
+      this.신고수[e] += 1;
+    },
   },
   components: {
   }
