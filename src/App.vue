c8ae078b6f8d5b39192533b13454fd3b3590f725
songhajang <angpan2004@gmail.com>
2022-08-08 16:23:11 +0900

    데이터바인딩 활용

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img src="./assets/logo.png" alt="">
+    <h1>{{logo}}</h1>
+    <div>
+      <h4 :style="color">{{products[0]}}</h4>
+      <p>{{price[0]}} 만원</p>
+    </div>
+    <div>
+      <h4 :style="color">{{products[1]}}</h4>
+      <p>{{price[1]}} 만원</p>
+    </div>
+    <div>
+      <h4 :style="color">{{products[2]}}</h4>
+      <p>{{price[2]}} 만원</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+        logo : '원룸샵',
+        color : 'color : red;',
+        products : ['역삼동원룸','청호동원룸','마포구원룸'],
+        price : [60,70,80],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
