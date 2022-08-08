3d1dd779b4ab7d34f8393711be824300bc3fe42f
songhajang <angpan2004@gmail.com>
2022-08-08 20:54:38 +0900

    Component Discount

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,12 @@
     <!-- 메뉴바 -->
     <div class="menu">
       <!-- <a v-for="menuLi in 3" :key="menuLi">HOME</a> -->
-
       <a v-for="(menuLi, idx) in menu" :key="idx">{{menuLi}}</a>
-
     </div>
 
-    <div class="black-bg" v-if="modal == true">
-      <div class="white-bg">
-        <img :src="datas[useClick].image" alt="">
-        <h4>{{datas[useClick].title}}</h4>
-        <p>{{datas[useClick].content}}</p>
-        <p>{{datas[useClick].price}} 원</p>
-        <button @click="modal = false">닫기</button>
-      </div>
-    </div>
+    <main-discount />
 
+ 
     <div v-for="(data, key) in datas" :key="key">
       <img :src="data.image" class="room-img">
       <h4 @click="modal = true; useClick = key ">{{data.title}}</h4>
@@ -30,6 +21,7 @@
 <script>
 
 import  dataList from './components/oneroom.js';
+import MainDiscount from './components/MainDiscount.vue';
 
 export default {
   name: 'App',
@@ -48,7 +40,8 @@ export default {
     },
   },
   components: {
-  }
+    MainDiscount,
+}
 }
 </script>
 
@@ -58,32 +51,9 @@ export default {
 }
 body{
   margin: 0;
-}
-div{
-  box-sizing: border-box;
-}
-.black-bg {
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  position: fixed;
-  top: 0;
-  padding: 20px;
-}
-.white-bg{
-  width: 100%;
-  background: white;
-  border-radius: 8px;
-  padding: 20px;
+  text-align: center;
 }
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 .menu{
   background: darkslateblue;
   padding: 15px;
